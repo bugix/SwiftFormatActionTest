0b45507e761d24cf83b988368bfba6ab58f47dc2
Martin Imobersteg <martin.imobersteg@gmail.com>
2025-01-09 07:31:32 +0100

    Add Foo

---
+++ SwiftFormatActionTest/Foo.swift
@@ -0,0 +1,21 @@
+//
+//  Foo.swift
+//  SwiftFormatActionTest
+//
+//  Created by Martin Imobersteg on 09.01.2025.
+//
+
+import Foundation
+
+public final class Foo {
+    
+    @_spi(Internals)
+    public func scope<FooBar, FizzBuzz>(
+        id: String,
+        action fromChildAction: @escaping (FooBar) -> FizzBuzz
+    )
+    {
+        print("ohhi")
+    }
+    
+}
