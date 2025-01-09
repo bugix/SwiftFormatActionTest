36275396ba9e8fdfde6a0617c6de67d23455046d
Martin Imobersteg <martin.imobersteg@gmail.com>
2025-01-09 07:21:26 +0100

    init

---
+++ SwiftFormatActionTest/SwiftFormatActionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFormatActionTestApp.swift
+//  SwiftFormatActionTest
+//
+//  Created by Martin Imobersteg on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFormatActionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftFormatActionTestTests/SwiftFormatActionTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFormatActionTestTests.swift
+//  SwiftFormatActionTestTests
+//
+//  Created by Martin Imobersteg on 09.01.2025.
+//
+
+import Testing
+@testable import SwiftFormatActionTest
+
+struct SwiftFormatActionTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
