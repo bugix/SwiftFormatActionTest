fe20fa49ee4cc88742dda591c725380d372fb8f1
bugix <bugix@users.noreply.github.com>
2025-01-09 08:06:57 +0000

    Run swift-format

--- SwiftFormatActionTest/Foo.swift
+++ SwiftFormatActionTest/Foo.swift
@@ -10,11 +10,10 @@ import Foundation
 public final class Foo {
 
   @_spi(Internals)
-  public  func scope<FooBar, FizzBuzz>(
-      id: String,
-      action fromChildAction: @escaping (FooBar) -> FizzBuzz
-    )
-  {
+  public func scope<FooBar, FizzBuzz>(
+    id: String,
+    action fromChildAction: @escaping (FooBar) -> FizzBuzz
+  ) {
     print("ohhi")
   }
 
