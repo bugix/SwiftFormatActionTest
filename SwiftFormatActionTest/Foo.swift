a5bc7377bfd5c942d9c306e1baa49742fee58a01
Martin Imobersteg <martin.imobersteg@gmail.com>
2025-01-09 09:05:02 +0100

    Destroy formating

--- SwiftFormatActionTest/Foo.swift
+++ SwiftFormatActionTest/Foo.swift
@@ -10,11 +10,12 @@ import Foundation
 public final class Foo {
 
   @_spi(Internals)
-  public func scope<FooBar, FizzBuzz>(
-    id: String,
-    action fromChildAction: @escaping (FooBar) -> FizzBuzz
-  ) {
-    print("ohhi")
+  public
+    func scope<FooBar, FizzBuzz>(
+        id: String,
+        action fromChildAction: @escaping (FooBar) -> FizzBuzz
+    ) {
+        print("ohhi")
   }
 
 }
